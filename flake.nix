6ffc56cfa18c8b47653cb6eb67b97c14f467bb12
Zach Coyle <zach.coyle@gmail.com>
2023-10-23 17:37:59 -0400

    add alejandra

--- flake.nix
+++ flake.nix
@@ -1,30 +1,40 @@
 {
-  description = "Example Darwin system flake";
+  description = "Darwin system flake";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nix-darwin.url = "github:LnL7/nix-darwin";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+      url = github:nix-community/home-manager;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
-  let
-    configuration = { pkgs, ... }: {
+  outputs = inputs @ {
+    self,
+    nix-darwin,
+    nixpkgs,
+    home-manager,
+  }: let
+    configuration = {pkgs, ...}: {
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        with pkgs; [
-          neovim
-          python3
-          poetry
-          alacritty
-          nodejs_20
-          nodePackages_latest.pnpm
-          podman
-          podman-tui
-          qemu
-          swift-format
-        ];
+      environment.systemPackages = with pkgs; [
+        neovim
+        python3
+        poetry
+        alacritty
+        nodejs_20
+        nodePackages_latest.pnpm
+        podman
+        podman-tui
+        qemu
+        swift-format
+        alejandra
+      ];
 
       # Auto upgrade nix package and the daemon service.
       services.nix-daemon.enable = true;
@@ -34,7 +44,7 @@
       nix.settings.experimental-features = "nix-command flakes";
 
       # Create /etc/zshrc that loads the nix-darwin environment.
-      programs.zsh.enable = true;  # default shell on catalina
+      programs.zsh.enable = true; # default shell on catalina
       # programs.fish.enable = true;
 
       # Set Git commit hash for darwin-version.
@@ -47,15 +57,16 @@
       # The platform the configuration will be used on.
       nixpkgs.hostPlatform = "x86_64-darwin";
     };
-  in
-  {
+  in {
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#Zacharys-MacBook-Pro
     darwinConfigurations."Zacharys-MacBook-Pro" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
+      modules = [configuration];
     };
 
     # Expose the package set, including overlays, for convenience.
     darwinPackages = self.darwinConfigurations."Zacharys-MacBook-Pro".pkgs;
+
+    formatter.x86_64-darwin = nixpkgs.legacyPackages.x86_64-darwin.alejandra;
   };
 }
