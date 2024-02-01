2b17f1d74b88cf02c58f4e15f9bf0bb567fcc770
Zach Coyle <zach.coyle@gmail.com>
2024-02-01 14:42:07 -0500

    waybar updates

--- users/zcoyle/by-app/waybar/default.nix
+++ users/zcoyle/by-app/waybar/default.nix
@@ -12,6 +12,9 @@
         "hyprland/workspaces"
       ];
       modules-right = [
+        "cpu"
+        "memory"
+        "disk"
         "pulseaudio"
         "bluetooth"
         "network"
@@ -20,6 +23,58 @@
       ];
       "hyprland/window" = {
         icon = true;
+      };
+
+      battery = {
+        states = {
+          # good = 95;
+          warning = 30;
+          critical = 15;
+        };
+        format = "{icon}   {capacity}%";
+        "format-charging" = "  {capacity}%";
+        "format-plugged" = "  {capacity}%";
+        "format-alt" = "{icon}  {time}";
+        # "format-good" = ""; # An empty format will hide the module
+        # "format-full" = "";
+        "format-icons" = [" " " " " " " " " "];
+      };
+
+      clock = {
+        timezone = "America/Indiana/Indianapolis";
+        format = "{:%I:%M}";
+        "format-alt" = "{:%Y-%m-%d %I:%M}";
+        "tooltip-format" = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
+      };
+
+      cpu = {
+        format = "󰻠 {usage}% ";
+        "on-click" = "alacritty -e btm";
+      };
+
+      disk = {
+        interval = 30;
+        format = "󰋊 {percentage_used}% ";
+        path = "/";
+        "on-click" = "alacritty -e btm";
+      };
+
+      memory = {
+        format = "󰍛 {}% ";
+        "on-click" = "alacritty -e btm";
+      };
+
+      network = {
+        format = "{ifname}";
+        format-wifi = "   {signalStrength}%";
+        format-ethernet = "  {ipaddr}";
+        "format-disconnected" = "Not connected"; #An empty format will hide the module.
+        "tooltip-format" = " {ifname} via {gwaddri}";
+        "tooltip-format-wifi" = "   {essid} ({signalStrength}%)";
+        "tooltip-format-ethernet" = "  {ifname} ({ipaddr}/{cidr})";
+        "tooltip-format-disconnected" = "Disconnected";
+        "max-length" = 50;
+        "on-click" = "alacritty -e nmtui";
       };
     };
   };
