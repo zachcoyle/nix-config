92db045c415664ed869f5b45017db395c1bb8e65
Zach Coyle <zach.coyle@gmail.com>
2024-02-02 21:19:53 -0500

    rearrange waybar

--- users/zcoyle/by-app/waybar/default.nix
+++ users/zcoyle/by-app/waybar/default.nix
@@ -9,11 +9,11 @@
       margin = "4";
 
       modules-left = [
+        "tray"
         "hyprland/window"
       ];
 
       modules-center = [
-        "tray"
         "hyprland/workspaces"
       ];
 
@@ -21,7 +21,6 @@
         "cpu"
         "memory"
         "disk"
-        "custom/nixos"
         "pulseaudio"
         "bluetooth"
         "network"
@@ -65,31 +64,6 @@
       cpu = {
         format = "󰻠 {usage}% ";
         "on-click" = "alacritty -e btm";
-      };
-
-      "custom/nixos" = {
-        format = "󱄅 {}";
-        return-type = "json";
-        restart-interval = 60;
-        on-click = "alacritty -e 'sudo nixos-rebuild switch --flake /home/zcoyle/Developer/github.com/zachcoyle/config'";
-        exec = "${pkgs.writeScriptBin "nix-fresh.sh" ''
-          threshhold_green=0
-          threshhold_yellow=25
-          threshhold_red=100
-          updates=$(nixos-rebuild dry-build --flake . 2>  >(grep -i "/nix/store" | wc -l))
-          css_class="green"
-          if [ "$updates" -gt $threshhold_yellow ]; then
-              css_class="yellow"
-          fi
-          if [ "$updates" -gt $threshhold_red ]; then
-              css_class="red"
-          fi
-          if [ "$updates" -gt $threshhold_green ]; then
-              printf '{"text": "%s", "alt": "%s", "tooltip": "%s Updates", "class": "%s"}' "$updates" "$updates" "$updates" "$css_class"
-          else
-              printf '{"text": "0", "alt": "0", "tooltip": "0 Updates", "class": "green"}'
-          fi
-        ''}/bin/nix-fresh.sh";
       };
 
       disk = {
