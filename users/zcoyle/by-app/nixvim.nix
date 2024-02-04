0ff35ec55bfd0af334bd97945f350f374893bbe9
Zach Coyle <zach.coyle@gmail.com>
2024-02-04 10:18:31 -0500

    add keybinds for ollama

--- users/zcoyle/by-app/nixvim.nix
+++ users/zcoyle/by-app/nixvim.nix
@@ -453,6 +453,22 @@ in {
         desc = "Open Navbuddy";
       };
     }
+    {
+      key = "<leader>oo";
+      action = ":<c-u>lua require('ollama').prompt()<cr>";
+      options = {
+        desc = "ollama prompt";
+      };
+      mode = ["n" "v"];
+    }
+    {
+      key = "<leader>oG";
+      action = ":<c-u>lua require('ollama').prompt('Generate_Code')<cr>";
+      options = {
+        desc = "ollama Generate Code";
+      };
+      mode = ["n" "v"];
+    }
   ];
 
   colorschemes = {
