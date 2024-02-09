65416c50a565122119eca30dc2f5604a9a8326ef
Zach Coyle <zach.coyle@gmail.com>
2024-02-09 13:03:57 -0500

    allow repeat on window resize binds

--- users/zcoyle/by-app/Hyprland/default.nix
+++ users/zcoyle/by-app/Hyprland/default.nix
@@ -77,10 +77,10 @@ in {
     bind = SUPERSHIFT, K, swapwindow, u
     bind = SUPERSHIFT, L, swapwindow, r
 
-    bind = ALT, L, resizeactive, 20 0
-    bind = ALT, H, resizeactive, -20 0
-    bind = ALT, K, resizeactive, 0 -20
-    bind = ALT, J, resizeactive, 0 20
+    binde = ALT, L, resizeactive, 10 0
+    binde = ALT, H, resizeactive, -10 0
+    binde = ALT, K, resizeactive, 0 -10
+    binde = ALT, J, resizeactive, 0 10
 
     bind = SUPERALT, L, movetoworkspacesilent, +1
     bind = SUPERALT, H, movetoworkspacesilent, -1
