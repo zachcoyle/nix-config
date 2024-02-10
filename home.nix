1489ee9cae728712e2796e041a95df9a9ae3b181
Zach Coyle <zach.coyle@gmail.com>
2024-02-10 17:00:59 -0500

    Revert "update firefox settings"
    This reverts commit b1b7c2376df8fb613ccad4887d2cfd7a54d3453e.

--- home.nix
+++ home.nix
@@ -376,91 +376,18 @@ in {
 
     firefox = {
       enable = pkgs.stdenv.isLinux;
-      package = pkgs.wrapFirefox pkgs.firefox-unwrapped {
-        extraPolicies = {
-          DisableFirefoxStudies = true;
-          DisablePocket = true;
-          NoDefaultBookmarks = true;
-          FirefoxHome = {
-            Search = true;
-            Pocket = false;
-            TopSites = false;
-            Highlights = false;
-          };
-          UserMessaging = {
-            ExtensionRecommendations = false;
-            SkipOnboarding = true;
-          };
-        };
-      };
-
-      extensions = with pkgs.nur.repos.rycee.firefox-addons; [
-        firenvim
-        react-devtools
-        reduxdevtools
-        stylus
-        ublock-origin
-        vimium
-        vue-js-devtools
-        wayback-machine
-      ];
-
       profiles.zcoyle = {
-        id = 0;
-        name = "zcoyle";
-        search = {
-          force = true;
-          default = "DuckDuckGo";
-          engines = {
-            "Nix Packages" = {
-              urls = [
-                {
-                  template = "https://search.nixos.org/packages";
-                  params = [
-                    {
-                      name = "type";
-                      value = "packages";
-                    }
-                    {
-                      name = "query";
-                      value = "{searchTerms}";
-                    }
-                  ];
-                }
-              ];
-              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-              definedAliases = ["@np"];
-            };
-            "NixOS Wiki" = {
-              urls = [{template = "https://nixos.wiki/index.php?search={searchTerms}";}];
-              iconUpdateURL = "https://nixos.wiki/favicon.png";
-              updateInterval = 24 * 60 * 60 * 1000;
-              definedAliases = ["@nw"];
-            };
-            "Wikipedia (en)".metaData.alias = "@wiki";
-            "Google".metaData.hidden = true;
-            "Amazon.com".metaData.hidden = true;
-            "Bing".metaData.hidden = true;
-            "eBay".metaData.hidden = true;
-          };
-
-          settings = {
-            "general.smoothScroll" = true;
-          };
-
-          extraConfig = ''
-            user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-            user_pref("full-screen-api.ignore-widgets", true);
-            user_pref("media.ffmpeg.vaapi.enabled", true);
-            user_pref("media.rdd-vpx.enabled", true);
-          '';
-
-          userChrome = ''
-          '';
-
-          userContent = ''
-          '';
-        };
+        extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+          firenvim
+          react-devtools
+          reduxdevtools
+          stylus
+          ublock-origin
+          vimium
+          vue-js-devtools
+          wayback-machine
+        ];
+        settings = {};
       };
     };
 
