c457cb853f75e5acd5064edef7f47d45209e391b
Zach Coyle <zach.coyle@gmail.com>
2024-02-14 22:59:49 -0500

    convert skhd config to nix

--- darwin.nix
+++ darwin.nix
@@ -1,4 +1,17 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: let
+  buildSkhdBinding = {
+    mods,
+    key,
+    command,
+    ...
+  }: ''
+    ${lib.concatStringsSep " + " mods} - ${key} : ${command}
+  '';
+in {
   nix = {
     linux-builder = {
       enable = true;
@@ -34,7 +47,179 @@
 
     skhd = {
       enable = true;
-      skhdConfig = builtins.readFile ./dots/skhdrc;
+      # TODO: make these consistent with hyprland
+      skhdConfig = lib.concatStrings (builtins.map buildSkhdBinding [
+        {
+          mods = ["fn" "shift"];
+          key = "q";
+          command = ''launchctl kickstart -k "gui/''${UID}/homebrew.mxcl.yabai"; skhd --reload'';
+        }
+        # Move focus
+        {
+          mods = ["fn"];
+          key = "h";
+          command = "yabai -m window --focus west";
+        }
+        {
+          mods = ["fn"];
+          key = "j";
+          command = "yabai -m window --focus south";
+        }
+        {
+          mods = ["fn"];
+          key = "k";
+          command = "yabai -m window --focus north";
+        }
+        {
+          mods = ["fn"];
+          key = "l";
+          command = "yabai -m window --focus east";
+        }
+        # Swap
+        {
+          mods = ["fn" "shift"];
+          key = "h";
+          command = "yabai -m window --swap west";
+        }
+        {
+          mods = ["fn" "shift"];
+          key = "j";
+          command = "yabai -m window --swap south";
+        }
+        {
+          mods = ["fn" "shift"];
+          key = "k";
+          command = "yabai -m window --swap north";
+        }
+        {
+          mods = ["fn" "shift"];
+          key = "l";
+          command = "yabai -m window --swap east";
+        }
+        # Move
+        {
+          mods = ["shift" "cmd"];
+          key = "h";
+          command = "yabai -m window --warp west";
+        }
+        {
+          mods = ["shift" "cmd"];
+          key = "j";
+          command = "yabai -m window --warp south";
+        }
+        {
+          mods = ["shift" "cmd"];
+          key = "k";
+          command = "yabai -m window --warp north";
+        }
+        {
+          mods = ["shift" "cmd"];
+          key = "l";
+          command = "yabai -m window --warp east";
+        }
+        # Balance
+        {
+          mods = ["shift" "alt"];
+          key = "0";
+          command = "yabai -m space --balance";
+        }
+        # Fill
+        {
+          mods = ["shift" "alt"];
+          key = "up";
+          command = "yabai -m window --grid 1:1:0:0:1:1";
+        }
+        # create desktop, move window and follow focus
+        {
+          mods = ["shift" "cmd"];
+          key = "n";
+          # FIXME:
+          command = lib.concatStringsSep "\n" [
+            ''yabai -m space --create && \''
+            ''index="''$(yabai -m query --spaces --display | jq 'map(select(."is-native-fullscreen" == false))[-1].index')" && \''
+            ''yabai -m window --space "''${index}" && \''
+            ''yabai -m space --focus "''${index}''
+          ];
+        }
+        # Focus
+        {
+          mods = ["cmd" "alt"];
+          key = "x";
+          command = "yabai -m space --focus recent";
+        }
+        {
+          mods = ["cmd" "alt"];
+          key = "1";
+          command = "yabai -m space --focus 1";
+        }
+        # Move to space and follow focus
+        {
+          mods = ["shift" "cmd"];
+          key = "2";
+          command = "yabai -m window --space 2; yabai -m space --focus 2";
+        }
+        # Focus monitor
+        {
+          mods = ["ctrl" "alt"];
+          key = "z";
+          command = "yabai -m display --focus prev";
+        }
+        {
+          mods = ["ctrl" "alt"];
+          key = "3";
+          command = "yabai -m display --focus 3";
+        }
+        {
+          mods = ["ctrl" "cmd"];
+          key = "c";
+          command = "yabai -m window --display next; yabai -m display --focus next";
+        }
+        {
+          mods = ["shift" "ctrl"];
+          key = "a";
+          command = "yabai -m window --move rel:-20:0";
+        }
+        {
+          mods = ["shift" "ctrl"];
+          key = "s";
+          command = "yabai -m window --move rel:0:20";
+        }
+        {
+          mods = ["shift" "alt"];
+          key = "a";
+          command = "yabai -m window --resize left:-20:0";
+        }
+        {
+          mods = ["shift" "alt"];
+          key = "w";
+          command = "yabai -m window --resize top:0:-20";
+        }
+        {
+          mods = ["alt"];
+          key = "d";
+          command = "yabai -m window --toggle zoom-parent";
+        }
+        {
+          mods = ["alt"];
+          key = "f";
+          command = "yabai -m window --toggle zoom-fullscreen";
+        }
+        {
+          mods = ["alt"];
+          key = "e";
+          command = "yabai -m window --toggle split";
+        }
+        {
+          mods = ["alt"];
+          key = "t";
+          command = "yabai -m window --toggle float --grid 4:4:1:1:2:2";
+        }
+        {
+          mods = ["alt"];
+          key = "p";
+          command = "yabai -m window --toggle sticky --toggle pip";
+        }
+      ]);
     };
 
     # tailscale.enable = true;
@@ -48,17 +233,17 @@
       enableScriptingAddition = true;
       ##############
       config = {
-        focus_follows_mouse = "off";
-        mouse_follows_focus = "off";
+        focus_follows_mouse = "on";
+        mouse_follows_focus = "on";
         window_placement = "second_child";
         window_opacity = "on";
         active_window_opacity = 1.0;
-        normal_window_opacity = 0.9;
-        top_padding = 3;
-        bottom_padding = 3;
-        left_padding = 3;
-        right_padding = 3;
-        window_gap = 3;
+        normal_window_opacity = 0.8;
+        top_padding = 10;
+        bottom_padding = 10;
+        left_padding = 10;
+        right_padding = 10;
+        window_gap = 10;
         layout = "bsp";
       };
       extraConfig = ''
