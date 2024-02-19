425813ff2e0cf771746623cbd39ca1c0977184f0
Zach Coyle <zach.coyle@gmail.com>
2024-02-18 19:03:14 -0500

    add some laptop power saving settings

--- hosts/nixos/nixos-laptop/configuration.nix
+++ hosts/nixos/nixos-laptop/configuration.nix
@@ -24,4 +24,44 @@
   };
 
   networking.hostName = "nixos-laptop"; # Define your hostname.
+
+  # https://nixos.wiki/wiki/Laptop
+
+  powerManagement.enable = true;
+
+  services = {
+    thermald.enable = true;
+
+    tlp = {
+      enable = true;
+      settings = {
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+        CPU_MIN_PERF_ON_AC = 0;
+        CPU_MAX_PERF_ON_AC = 100;
+        CPU_MIN_PERF_ON_BAT = 0;
+        CPU_MAX_PERF_ON_BAT = 20;
+
+        #Optional helps save long term battery health
+        START_CHARGE_THRESH_BAT0 = 40; # 40 and below it starts to charge
+        STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
+      };
+    };
+
+    auto-cpufreq.enable = true;
+    auto-cpufreq.settings = {
+      battery = {
+        governor = "powersave";
+        turbo = "never";
+      };
+      charger = {
+        governor = "performance";
+        turbo = "auto";
+      };
+    };
+  };
 }
