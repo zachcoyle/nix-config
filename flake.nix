5609b661669a7c21758d013eb8c68c6b68a4b88e
Zach Coyle <zach.coyle@gmail.com>
2024-02-26 18:59:57 -0500

    unpin nixpkgs

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # system
-    nixpkgs.url = "github:NixOS/nixpkgs/48b75eb6e521f2303cb3cd53a94ec80021b422aa";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
     nix-darwin = {
       url = "github:LnL7/nix-darwin";
