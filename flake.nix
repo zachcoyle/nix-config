69e2863cf5ce0fba2fd5e044c8a88907e9713f9b
Zach Coyle <zach.coyle@gmail.com>
2024-02-27 18:05:11 -0500

    revert a001e2f1179fb889aeaf2ecdfafc9b0669964d9e

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   description = "nixos + darwin system flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    # https://nixpk.gs/pr-tracker.html?pr=285828
+    nixpkgs.url = "github:NixOS/nixpkgs/48b75eb6e521f2303cb3cd53a94ec80021b422aa";
 
     nix-darwin = {
       url = "github:LnL7/nix-darwin";
@@ -17,9 +18,7 @@
     };
 
     nixvim = {
-      # https://github.com/nix-community/nixvim/pull/1163/files
-      # url = "github:nix-community/nixvim";
-      url = "github:traxys/nixvim/codesniffer";
+      url = "github:nix-community/nixvim";
       inputs = {
         nixpkgs.follows = "nixpkgs";
         pre-commit-hooks.follows = "pre-commit-hooks";
