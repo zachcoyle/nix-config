782ceb9234cf4b3bc0d7272886e0e8662a37e0b2
Zach Coyle <zach.coyle@gmail.com>
2024-02-29 13:49:54 -0500

    go around stylix to force alacritty to have opacity on darwin

--- users/zcoyle/by-app/alacritty.nix
+++ users/zcoyle/by-app/alacritty.nix
@@ -4,40 +4,46 @@
   ...
 }: {
   enable = true;
-  settings = {
-    window = {
-      decorations_theme_variant = "Dark";
-      blur = true;
-      padding = {
-        x = 4;
-        y = 0;
+  settings =
+    lib.recursiveUpdate {
+      window = {
+        decorations_theme_variant = "Dark";
+        blur = true;
+        padding = {
+          x = 4;
+          y = 0;
+        };
       };
-    };
-    font = {
-      # normal.family = "FiraCode Nerd Font";
-      # size = 10.0;
-    };
-    mouse.hide_when_typing = true;
-    keyboard.bindings =
-      [
-        {
-          key = "V";
-          mods = "Super";
-          action = "Paste";
-        }
-        {
-          key = "C";
-          mods = "Super";
-          action = "Copy";
-        }
-      ]
-      ++ lib.optionals pkgs.stdenv.isDarwin [
-        {
-          key = "T";
-          mods = "Command";
-          action = "CreateNewTab";
-        }
-      ];
-    # import = ["${pkgs.alacritty-theme}/gruvbox_dark.toml"];
-  };
+      font = {
+        # normal.family = "FiraCode Nerd Font";
+        # size = 10.0;
+      };
+      mouse.hide_when_typing = true;
+      keyboard.bindings =
+        [
+          {
+            key = "V";
+            mods = "Super";
+            action = "Paste";
+          }
+          {
+            key = "C";
+            mods = "Super";
+            action = "Copy";
+          }
+        ]
+        ++ lib.optionals pkgs.stdenv.isDarwin [
+          {
+            key = "T";
+            mods = "Command";
+            action = "CreateNewTab";
+          }
+        ];
+      # import = ["${pkgs.alacritty-theme}/gruvbox_dark.toml"];
+    }
+    (
+      if pkgs.stdenv.isDarwin
+      then {window.opacity = lib.mkForce 0.8;}
+      else {}
+    );
 }
