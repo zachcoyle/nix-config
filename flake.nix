276ff480d88737fde640a23f816703fd463131cd
Zach Coyle <zach.coyle@gmail.com>
2024-03-09 19:50:48 -0500

    adjust swww for desktop

--- flake.nix
+++ flake.nix
@@ -278,10 +278,10 @@
                 # TODO: host-specific home file
                 home-manager.users.zcoyle.wayland.windowManager.hyprland.settings = {
                   exec-once = [
-                    "swww img ${wallpaperDir}/platform.jpg --transition-fps 60 --transition-type grow --transition-pos 1470,630"
+                    "swww img ${wallpaperDir}/platform.jpg --transition-fps 60 --transition-type grow --transition-pos 1695,855"
                   ];
                   bind = [
-                    "SUPER, P, exec, swww img ${wallpaperDir}/`ls ${wallpaperDir} | shuf -n 1` --transition-fps 60 --transition-type grow --transition-pos 1470,630"
+                    "SUPER, P, exec, swww img ${wallpaperDir}/`ls ${wallpaperDir} | shuf -n 1` --transition-fps 60 --transition-type grow --transition-pos 1695,855"
                   ];
                   monitor = [
                     "HDMI-A-1, 1920x1080@60, 0x0, 1"
