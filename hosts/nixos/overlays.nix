3a904a239004c583066d5f98e03df25fbed08f05
Zach Coyle <zach.coyle@gmail.com>
2024-03-13 11:04:35 -0400

    reorder hypr* overlays

--- hosts/nixos/overlays.nix
+++ hosts/nixos/overlays.nix
@@ -1,8 +1,8 @@
 {inputs, ...}: {
   nixpkgs.overlays = [
     inputs.sddm-sugar-candy-nix.overlays.default
-    inputs.hyprland.overlays.default
     inputs.hypridle.overlays.default
     inputs.hyprlock.overlays.default
+    inputs.hyprland.overlays.default
   ];
 }
