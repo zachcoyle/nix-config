7b168aab32e93f9a430cbfd0167c44f99f997871
Zach Coyle <zach.coyle@gmail.com>
2024-03-17 11:13:02 -0400

    add firefox back to darwin

--- home.nix
+++ home.nix
@@ -157,6 +157,151 @@ in {
       '';
     };
 
+    firefox = {
+      enable = true;
+      package =
+        pkgs.wrapFirefox (
+          if pkgs.stdenv.isDarwin
+          then pkgs.firefox-bin
+          else pkgs.firefox-unwrapped
+        ) {
+          extraPolicies = {
+            DisableFirefoxStudies = true;
+            DisablePocket = true;
+            NoDefaultBookmarks = true;
+            FirefoxHome = {
+              Search = true;
+              Pocket = false;
+              TopSites = false;
+              Highlights = false;
+            };
+            UserMessaging = {
+              ExtensionRecommendations = false;
+              SkipOnboarding = true;
+            };
+          };
+        };
+
+      profiles.zcoyle = {
+        id = 0;
+        name = "zcoyle";
+        extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+          firenvim
+          react-devtools
+          reddit-enhancement-suite
+          reduxdevtools
+          stylus
+          ublock-origin
+          vimium
+          vue-js-devtools
+          wayback-machine
+        ];
+
+        search = {
+          force = true;
+          default = "Brave";
+          engines = {
+            Brave = {
+              urls = [{template = "https://search.brave.com/search?q={searchTerms}";}];
+              iconUpdateURL = "https://cdn.search.brave.com/serp/v2/_app/immutable/assets/brave-search-icon.CsIFM2aN.svg";
+              updateInterval = 24 * 60 * 60 * 1000;
+              definedAliases = ["@b"];
+            };
+            "Nix Packages" = {
+              urls = [
+                {
+                  template = "https://search.nixos.org/packages";
+                  params = [
+                    {
+                      name = "channel";
+                      value = "unstable";
+                    }
+                    {
+                      name = "type";
+                      value = "packages";
+                    }
+                    {
+                      name = "query";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
+              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              definedAliases = ["@np"];
+            };
+            "NixOS Options" = {
+              urls = [
+                {
+                  template = "https://search.nixos.org/options";
+                  params = [
+                    {
+                      name = "channel";
+                      value = "unstable";
+                    }
+                    {
+                      name = "type";
+                      value = "packages";
+                    }
+                    {
+                      name = "query";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
+              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              definedAliases = ["@no"];
+            };
+            "NixOS Wiki" = {
+              urls = [{template = "https://nixos.wiki/index.php?search={searchTerms}";}];
+              iconUpdateURL = "https://nixos.wiki/favicon.png";
+              updateInterval = 24 * 60 * 60 * 1000;
+              definedAliases = ["@nw"];
+            };
+            "Ollama" = {
+              urls = [{template = "https://ollama.com/search?q={searchTerms}";}];
+              iconUpdateURL = "https://ollama.com/public/icon-32x32.png";
+              updateInterval = 24 * 60 * 60 * 1000;
+              definedAliases = ["@ll"];
+            };
+            "Wikipedia (en)".metaData.alias = "@wiki";
+            "Google".metaData.hidden = true;
+            "Amazon.com".metaData.hidden = true;
+            "Bing".metaData.hidden = true;
+            "eBay".metaData.hidden = true;
+          };
+        };
+
+        settings = {
+          "general.smoothScroll" = true;
+          # disable alt key bringing up window menu
+          "ui.key.menuAccessKeyFocuses" = false;
+        };
+
+        extraConfig = ''
+          user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+          user_pref("full-screen-api.ignore-widgets", true);
+          user_pref("media.ffmpeg.vaapi.enabled", true);
+          user_pref("media.rdd-vpx.enabled", true);
+          user_pref("apz.overscroll.enabled", true);
+          user_pref("browser.shell.checkDefaultBrowser", false);
+        '';
+
+        userChrome = ''
+          .titlebar-buttonbox-container {
+            display: none !important;
+          }
+          statuspanel[type="overLink"] .statuspanel-label {
+            display: none !important;
+          }
+        '';
+
+        userContent = ''
+        '';
+      };
+    };
+
     git = {
       enable = true;
       package = pkgs.gitSVN;
