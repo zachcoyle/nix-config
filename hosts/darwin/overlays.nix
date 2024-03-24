265d1acc63f41e8018f803535049265ad7c6ebdd
Zach Coyle <zach.coyle@gmail.com>
2024-03-24 15:16:53 -0400

    use nixpkgs jankyborders

--- hosts/darwin/overlays.nix
+++ hosts/darwin/overlays.nix
@@ -4,10 +4,6 @@
     inputs.nixpkgs-firefox-darwin.overlay
     (_: prev: {
       inherit (inputs.nixpkgs-23-05-darwin.legacyPackages.${prev.system}) neovide;
-      # FIXME: this is in nixpkgs now. delete.
-      jankyborders = prev.darwin.apple_sdk_11_0.callPackage ../../packages/jankyborders.nix {
-        inherit (prev.darwin.apple_sdk_11_0.frameworks) AppKit CoreGraphics CoreVideo SkyLight;
-      };
     })
   ];
 }
