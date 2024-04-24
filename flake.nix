7da8fb08153fae51e3a84a9d1d5bfefbc177bb0f
Zach Coyle <zach.coyle@gmail.com>
2024-04-24 13:45:14 -0400

    move back to nixos-unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "nixos + darwin system flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     nix-darwin = {
       url = "github:LnL7/nix-darwin";
