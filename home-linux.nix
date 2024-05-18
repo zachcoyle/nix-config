3a64d0820b44d74127f90aa0306c5a48d6531cea
Zach Coyle <zach.coyle@gmail.com>
2024-05-17 21:52:54 -0400

    tweak number of rows in rofi

--- home-linux.nix
+++ home-linux.nix
@@ -266,7 +266,7 @@ in {
         };
         listview = {
           spacing = mkLiteral "10px";
-          lines = 8;
+          lines = 10;
         };
         element = {
           spacing = mkLiteral "10px";
