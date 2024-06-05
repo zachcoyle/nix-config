afd5ddcf3a4a1463183d1a1f613f71eb75c4b59f
Zach Coyle <zach.coyle@gmail.com>
2024-06-05 00:18:24 -0400

    add zramSwap

--- hosts/nixos/common-configuration.nix
+++ hosts/nixos/common-configuration.nix
@@ -243,6 +243,8 @@
     };
   };
 
+  zramSwap.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
