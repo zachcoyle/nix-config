9e8ed639083151343ceb2cc311996e17f6073703
Zach Coyle <zach.coyle@gmail.com>
2024-06-06 23:06:16 -0400

    temp commit

--- users/zcoyle/dots/ags/Workspaces.js
+++ users/zcoyle/dots/ags/Workspaces.js
@@ -1,8 +1,20 @@
-const Workspace = ({ /** @type{string} */ name }) =>
+const WorkspaceButton = ({
+  /** @type{number} */ id,
+  /** @type{string} */ label,
+}) => Widget.Button({});
+
+const Workspace = ({ /** @type{number} */ id, /** @type{string} */ label }) =>
   Widget.Box({
     children: [
       Widget.Label({
-        label: name,
+        label,
       }),
     ],
+  });
+
+const Workspaces2Inner = Widget.Box({});
+
+export const Workspaces2 = ({}) =>
+  Widget.EventBox({
+    child: Workspaces2Inner,
   });
