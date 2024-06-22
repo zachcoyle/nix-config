30194cfa9dbeae47758fd60b012cba50313b00f8
Zach Coyle <zach.coyle@gmail.com>
2024-06-22 12:41:51 -0400

    tweaks to overlays

--- flake.nix
+++ flake.nix
@@ -343,19 +343,21 @@
           nixos-overlays = {
             nixpkgs.overlays = [
               inputs.sddm-sugar-candy-nix.overlays.default
-              inputs.hypridle.overlays.default
-              inputs.hyprlock.overlays.default
-              inputs.hyprpicker.overlays.default
               inputs.hyprland.overlays.default
               inputs.neovim-nightly-overlay.overlays.default
               inputs.icon-themes-nightly-overlay.overlays.default
               inputs.xremap-flake.overlays.default
               (_: prev: {
-                logos = inputs.logos.packages.${prev.system}.default;
-                inherit (inputs.pyprland.packages.${prev.system}) pyprland;
                 inherit (inputs.ags.packages.${prev.system}) ags;
-                inherit (inputs.quickshell.packages.${prev.system}) quickshell;
+                inherit (inputs.hypridle.packages.${prev.system}) hypridle;
+                inherit (inputs.hyprlock.packages.${prev.system}) hyprlock;
+                inherit (inputs.hyprpicker.packages.${prev.system}) hyprpicker;
                 inherit (inputs.libastal.packages.${prev.system}) astal;
+                inherit (inputs.pyprland.packages.${prev.system}) pyprland;
+                inherit (inputs.quickshell.packages.${prev.system}) quickshell;
+
+                logos = inputs.logos.packages.${prev.system}.default;
+
                 # FIXME: https://github.com/NixOS/nixpkgs/issues/298539
                 rofi-calc = prev.rofi-calc.override { rofi-unwrapped = prev.rofi-wayland-unwrapped; };
                 rofi-emoji = prev.rofi-emoji.override { rofi-unwrapped = prev.rofi-wayland-unwrapped; };
