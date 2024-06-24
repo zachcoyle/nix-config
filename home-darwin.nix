88c48d612de3ce2e9554bfd8c399ff195dd321b4
Zach Coyle <zach.coyle@gmail.com>
2024-06-24 12:15:29 -0400

    darwin: fix attr name

--- home-darwin.nix
+++ home-darwin.nix
@@ -1,26 +1,28 @@
 { pkgs, ... }:
 {
-  home.file = {
-    "Library/Application\ Support/neovide/neovide-settings.json".text = import ./users/zcoyle/by-app/neovide.nix;
-    ".config/borders/bordersrc".executable = true;
-    ".config/borders/bordersrc".text = ''
-      #!/bin/bash
+  home = {
+    file = {
+      "Library/Application\ Support/neovide/neovide-settings.json".text = import ./users/zcoyle/by-app/neovide.nix;
+      ".config/borders/bordersrc".executable = true;
+      ".config/borders/bordersrc".text = ''
+        #!/bin/bash
 
-      options=(
-          style=round
-          width=6.0
-          hidpi=on
-          active_color=0xffebdbb2
-          inactive_color=0xff282828
-          background_color=0x302c2e34
-          blur_radius=25
-      )
+        options=(
+            style=round
+            width=6.0
+            hidpi=on
+            active_color=0xffebdbb2
+            inactive_color=0xff282828
+            background_color=0x302c2e34
+            blur_radius=25
+        )
 
-      borders "''${options[@]}"
-    '';
+        borders "''${options[@]}"
+      '';
+    };
+
+    packages = with pkgs; [ karabiner-elements ];
   };
-
-  packages = with pkgs; [ karabiner-elements ];
 
   programs = {
     firefox.package = pkgs.firefox-bin;
