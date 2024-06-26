bdd6a749527e90b9c0c0e4df4146746ddb94ab12
Zach Coyle <zach.coyle@gmail.com>
2024-06-26 16:26:32 -0400

    add laptop as remote builder for desktop

--- hosts/nixos/nixos-desktop/configuration.nix
+++ hosts/nixos/nixos-desktop/configuration.nix
@@ -1,11 +1,35 @@
-{pkgs, ...}: {
+{ pkgs, ... }:
+{
   imports = [
     ./hardware-configuration.nix
     ../common-configuration.nix
   ];
+
   boot = {
-    supportedFilesystems = ["bcachefs"];
+    supportedFilesystems = [ "bcachefs" ];
     kernelPackages = pkgs.linuxPackages_latest;
   };
+
   networking.hostName = "nixos-desktop";
+
+  nix = {
+    buildMachines = [
+      {
+        sshUser = "zcoyle";
+        hostName = "nixos-laptop";
+        system = "x86_64-linux";
+        protocol = "ssh-ng";
+        maxJobs = 6;
+        speedFactor = 0.5;
+        supportedFeatures = [
+          "nixos-test"
+          "benchmark"
+          "big-parallel"
+          "kvm"
+        ];
+        mandatoryFeatures = [ ];
+      }
+    ];
+    distributedBuilds = true;
+  };
 }
