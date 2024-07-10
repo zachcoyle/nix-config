4207f33b0f69a242b9becd9ce02ebdfeebce9a9b
Zach Coyle <zach.coyle@gmail.com>
2024-07-10 18:58:42 -0400

    darwin: add script to activate karabiner

---
+++ scripts/activate_karabiner.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+sudo /Applications/.Nix-Karabiner/.Karabiner-VirtualHIDDevice-Manager.app/Contents/MacOS/Karabiner-VirtualHIDDevice-Manager activate
+
+echo "System Preferences > Privacy & Security > Security > Allow"
