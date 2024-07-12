9e9e01cd29d8a06d53d371eca7c8ca58f7c290bb
Zach Coyle <zach.coyle@gmail.com>
2024-07-12 13:40:13 -0400

    atuin: fuzzy search

--- home.nix
+++ home.nix
@@ -129,7 +129,7 @@
         auto_sync = true;
         sync_frequency = "5m";
         sync_address = "http://nixos-desktop:8888";
-        search_mode = "prefix";
+        search_mode = "fuzzy";
         keymap_mode = "vim-normal";
       };
     };
