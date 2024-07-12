c81be45285931a98afa575b7e41c5ed6592f5bf0
Zach Coyle <zach.coyle@gmail.com>
2024-07-11 22:02:03 -0400

    cura: use appimage & freecad: disable

--- home-linux.nix
+++ home-linux.nix
@@ -102,11 +102,38 @@ in
       calibre
       cider
       copyq
-      cura
+      # INFO: https://github.com/NixOS/nixpkgs/issues/186570#issuecomment-1627797219
+      (
+        let
+          cura5 = appimageTools.wrapType2 rec {
+            name = "cura5";
+            version = "5.4.0";
+            src = fetchurl {
+              url = "https://github.com/Ultimaker/Cura/releases/download/${version}/UltiMaker-Cura-${version}-linux-modern.AppImage";
+              hash = "sha256-QVv7Wkfo082PH6n6rpsB79st2xK2+Np9ivBg/PYZd74=";
+            };
+            extraPkgs = pkgs: [ ];
+          };
+        in
+        writeScriptBin "cura" ''
+          #! ${pkgs.bash}/bin/bash
+          # AppImage version of Cura loses current working directory and treats all paths relateive to $HOME.
+          # So we convert each of the files passed as argument to an absolute path.
+          # This fixes use cases like `cd /path/to/my/files; cura mymodel.stl anothermodel.stl`.
+          args=()
+          for a in "$@"; do
+            if [ -e "$a" ]; then
+              a="$(realpath "$a")"
+            fi
+            args+=("$a")
+          done
+          exec "${cura5}/bin/cura5" "''${args[@]}"
+        ''
+      )
       element-desktop
       evince
       foliate
-      freecad
+      # freecad
       gnome-calculator
       gnome-font-viewer
       gnome-keyring
