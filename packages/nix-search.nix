6c2901b4d9748aa82b060da5134e4cc34038986f
Zach Coyle <zach.coyle@gmail.com>
2024-07-12 22:03:58 -0400

    add script to fuzzy find nixpkgs

---
+++ packages/nix-search.nix
@@ -0,0 +1,12 @@
+{ pkgs, ... }:
+pkgs.writeShellApplication {
+  name = "ns";
+  runtimeInputs = with pkgs; [
+    fzf
+    rippkgs
+  ];
+  text = ''
+    : | fzf --bind 'start:reload:rippkgs || true' \
+        --bind 'change:reload:rippkgs {q} || true'
+  '';
+}
