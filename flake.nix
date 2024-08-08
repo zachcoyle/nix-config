3866b4cf7659245d0138163551f7b4304d901621
Zach Coyle <zach.coyle@gmail.com>
2024-08-08 18:45:31 -0400

    use nixos-hardware fork

--- flake.nix
+++ flake.nix
@@ -187,8 +187,9 @@
       flake = false;
     };
 
-    nixos-hardware.url = "github:NixOS/nixos-hardware";
-    custom-nixos-hardware.url = "github:zachcoyle/nixos-hardware/t2-linux-6.10.3";
+    # nixos-hardware.url = "github:NixOS/nixos-hardware";
+    nixos-hardware.url = "github:zachcoyle/nixos-hardware/t2-linux-6.10.3";
+    # custom-nixos-hardware.url = "github:zachcoyle/nixos-hardware/t2-linux-6.10.3";
 
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
