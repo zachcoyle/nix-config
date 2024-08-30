35ee62c455eff4316ce013f9a84aadce0a3a0cb1
Zach Coyle <zach.coyle@gmail.com>
2024-08-30 00:08:33 -0400

    remove unneeded follows

--- flake.nix
+++ flake.nix
@@ -243,10 +243,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    crane = {
-      url = "github:ipetkov/crane";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    crane.url = "github:ipetkov/crane";
 
     hercules-ci-effects = {
       url = "github:hercules-ci/hercules-ci-effects";
