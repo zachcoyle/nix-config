21c6fff42f98fee1f25e6d97a6e4d1db1acde4c0
Zach Coyle <zach.coyle@gmail.com>
2024-08-29 20:59:44 -0400

    remove crane input

--- flake.nix
+++ flake.nix
@@ -163,7 +163,6 @@
         hyprland.follows = "hyprland";
         home-manager.follows = "home-manager";
         treefmt-nix.follows = "treefmt-nix";
-        crane.follows = "crane";
       };
     };
 
@@ -240,11 +239,6 @@
 
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    crane = {
-      url = "github:ipetkov/crane";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
@@ -454,14 +448,12 @@
                         auto-optimise-store = true;
                         use-xdg-base-directories = true;
                         substituters = [
-                          "https://crane.cachix.org"
                           "https://devenv.cachix.org"
                           "https://hyprland.cachix.org"
                           "https://nix-community.cachix.org"
                           "https://zachcoyle.cachix.org"
                         ];
                         trusted-public-keys = [
-                          "crane.cachix.org-1:8Scfpmn9w+hGdXH/Q9tTLiYAE/2dnJYRJP7kl80GuRk="
                           "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
                           "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
                           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
